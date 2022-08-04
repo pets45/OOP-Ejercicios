6f5a456cf1760b06cb802d8539935d746cfc48ce
Pedro RG <pets45@me.com>
2022-08-03 20:57:57 -0400

    initial commit

---
+++ challenge_OOP.py
@@ -0,0 +1,27 @@
+# Practicar creando/instanciando objetos
+
+class Perro:
+	raza: ''
+	size: ''
+	pelu: False
+
+
+	def ladrar(self, ladrido):
+		print(f'El perro ladra {ladrido}')
+
+	def __stl__(self):
+		return f'Perro raza {self.raza}, size: {self.size}, pelu: {self.pelu}'
+
+ladrido = 'uau! uau!'
+
+perrote = Perro()
+perrote.raza = 'Griffon Korthal'
+perrote.size = 'Mediano'
+perrote.pelu = True
+
+print(perrote)
+print(perrote.raza)
+print(perrote.size)
+print(perrote.pelu)
+
+perrote.ladrar(ladrido)
